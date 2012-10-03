3fde62e24096d38469a00ee0b0c867433770c402
Allen Lee <allenwlee130@gmail.com>
2012-10-02 19:45:32 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railroaded::Application.config.secret_token = 'e61a8e8aa7a35b25ac076ce25e37b9ec413a0ee60136d2cf060ee4cf4d14c936530f9f798cc71dcc4c8b839db78034c7e9f6fb2fd124e1fa8d4fbbaac476e7d4'
